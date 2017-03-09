f589d24b58b531af442232aa3e9df9d8302a7e1b
marcelo.correia <marcelo.correia@amaysim.com.au>
2017-03-09 11:04:17 +1100

    SP-316 branch 7.3.5-ready

--- framework.config.php
+++ framework.config.php
@@ -1,18 +1,18 @@
 <?php
-define('APP_DB_ENGINE','MyISAM');
-define('APP_DB_DATABASE','');
+define('APP_DB_ENGINE','innodb');
+define('APP_DB_DATABASE','cerb');
 define('APP_DB_PCONNECT',false);
 
-define('APP_DB_HOST','');
-define('APP_DB_USER','');
-define('APP_DB_PASS','');
+define('APP_DB_HOST','192.168.1.20');
+define('APP_DB_USER','cerb');
+define('APP_DB_PASS','secret_password');
 
 define('APP_DB_SLAVE_HOST','');
 define('APP_DB_SLAVE_USER','');
 define('APP_DB_SLAVE_PASS','');
 
-define('LANG_CHARSET_CODE','utf-8'); // utf-8, iso-8859-1
-define('DB_CHARSET_CODE','utf8'); // utf8, latin1
+define('LANG_CHARSET_CODE','utf-8');
+define('DB_CHARSET_CODE','utf8');
 
 //@ini_set('memory_limit', '64M');
 
@@ -27,7 +27,7 @@ define('APP_TEMP_PATH',APP_STORAGE_PATH . '/tmp');
 define('DEVBLOCKS_PATH',APP_PATH . '/libs/devblocks/');
 define('DEVBLOCKS_REWRITE', file_exists(dirname(__FILE__).'/.htaccess'));
 define('DEVELOPMENT_MODE', false);
-define('AUTHORIZED_IPS_DEFAULTS', '');
+define('AUTHORIZED_IPS_DEFAULTS','172.17.0.1');
 define('ONDEMAND_MODE', false);
 
 require_once(DEVBLOCKS_PATH . 'framework.defaults.php');
